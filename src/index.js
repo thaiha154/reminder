23d003b0cc124fe44385fb1920929509bf1d2ea6
thai <thaiha15499@gmail.com>
2017-10-24 09:52:56 +0700

    done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reminders from './reducers/reducer';
+import './App.css';
+
+const store = createStore(reminders);
+
+ReactDOM.render(
+  <Provider store ={store}>
+      <App />
+  </Provider>,
+  document.getElementById('root')
+);
